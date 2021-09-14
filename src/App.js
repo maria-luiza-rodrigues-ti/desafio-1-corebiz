dcc21fd9d4ae1da0b0a73a1f5889e6cea7178515
MariaLuizaBRF <marialuiza.r.eng@gmail.com>
2021-09-13 22:05:48 -0300

    build adicionado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/header/Navbar";
+import Slider from "./components/slider/Slider";
+import Form from './components/form/Form';
+import Footer from './components/footer/Footer';
+import Products from './components/products/Products'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Slider/>
+        <Products/>
+        <Form/>
+        <Footer/>
+      </Router>
+    </>
   );
 }
 
